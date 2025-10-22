a46df522db320db164dcfd2c71499da877940bd0
NishchayGithub2004 <162769065+NishchayGithub2004@users.noreply.github.com>
2025-10-22 13:21:10 +0530

    edited comments

--- job_portal/backend/middlewares/multer.js
+++ job_portal/backend/middlewares/multer.js
@@ -2,4 +2,5 @@ import multer from "multer"; // import the multer library to handle file uploads
 
 const storage = multer.memoryStorage(); // create a storage engine using multer's memoryStorage method to temporarily store uploaded files in memory as Buffer objects
 
-export const singleUpload = multer({ storage }).single("file"); // create and export a middleware named singleUpload by calling multer with the storage configuration and using the single method to handle uploads of a single file under the field name "file"
+export const singleUpload = multer({ storage }).single("file"); // create and export a middleware named 'singleUpload' by creating an object of 'multer'of name 'singleUpload'
+// specify that this middleware takes only one upload file at a time using 'single' method, and it takes the file from form field named 'file'
