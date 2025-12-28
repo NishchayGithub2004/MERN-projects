43a032d06b3a395948ee077ee83755ac34e6dac1
NishchayGithub2004 <162769065+NishchayGithub2004@users.noreply.github.com>
2025-12-28 20:34:34 +0530

    Delete insta_clone directory

--- insta_clone/backend/middlewares/multer.js
+++
@@ -1,7 +0,0 @@
-import multer from "multer"; // import the multer library to handle multipart/form-data, typically used for file uploads
-
-const upload = multer({ // define a multer instance named upload to configure file upload handling
-    storage: multer.memoryStorage(), // set the storage option to memoryStorage so uploaded files are stored in memory as Buffer objects instead of being written to disk
-});
-
-export default upload; // export the configured multer instance as the default export for use in routes or controllers handling file uploads
