3979c7b06c2c9922ebd5a3f480e0fecaefb0d66c
NishchayGithub2004 <162769065+NishchayGithub2004@users.noreply.github.com>
2026-02-14 07:35:32 +0530

    Delete e_commerce directory

--- e_commerce/backend/helpers/paypal.js
+++
@@ -1,9 +0,0 @@
-import paypal from "paypal-rest-sdk"; // import the official PayPal REST SDK to handle PayPal API operations like payment creation and execution
-
-paypal.configure({ // call the configure() method to initialize PayPal SDK with credentials and settings
-    mode: process.env.PAYPAL_MODE, // set the mode from environment variables; can be 'sandbox' for testing or 'live' for production
-    client_id: process.env.PAYPAL_CLIENT_ID, // set the PayPal client ID fetched from environment variables to identify the PayPal app
-    client_secret: process.env.PAYPAL_CLIENT_SECRET, // set the PayPal client secret fetched from environment variables to authenticate secure API calls
-});
-
-module.exports = paypal; // export the configured PayPal instance so it can be used in other modules for making API requests
--- e_commerce/backend/routes/shop/search-routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { searchProducts } from "../../controllers/shop/search-controller";
-
-const shopSearchRouter = express.Router();
-
-shopSearchRouter.get("/:keyword", searchProducts);
-
-module.exports = shopSearchRouter;
